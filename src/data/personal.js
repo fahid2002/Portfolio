dee66cc37453230bba3ade2047fd7f7e3b98dc13
fahid2002 <fahid23105101323.diu.edu.bd>
2026-05-11 23:50:39 +0600

    personal data

---
+++ src/data/personal.js
@@ -0,0 +1,33 @@
+export const personal = {
+  name:         'Fahid Hasan',
+  fullName:     'Fahid Hasan Khan',
+  logo:         'fahid.',
+  tagline:      'MERN Stack Developer & Designer',
+  designation:  [
+    'Software Engineer',
+    'MERN Stack Developer',
+    'Graphics Designer',
+    'UI/UX Designer',
+  ],
+  bio: "A CSE undergraduate at Daffodil International University — sailing the Grand Line of full-stack development. I build fast, beautiful web apps with React & Next.js, design perfect interfaces in Figma, and craft experiences users never forget.",
+  location:     'Dhaka, Bangladesh',
+  email:        'fahidhasanifty20@gmail.com',
+  phone:        '+88 01749573951',
+  whatsapp:     'https://wa.me/8801749573951',
+
+  // resume
+  resumeLink:   'GOOGLE_DRIVE_RESUME_LINK',
+
+  // profile picture
+  profileImage: '/images/profile.jpg',
+
+
+  //socials
+  socials: {
+    github:    'https://github.com/fahid2002', 
+    linkedin:  'www.linkedin.com/in/fahid-hasan-280425382', 
+    facebook:  'https://www.facebook.com/fh.ifty.1',
+    twitter:   'https://x.com/fh_ifty',    
+    instagram: 'https://www.instagram.com/fahidhasankhanifty/', 
+  },
+}
