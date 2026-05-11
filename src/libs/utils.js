a422a0494efe63a041e2bbbcdc36f850620a4908
fahid2002 <fahid23105101323.diu.edu.bd>
2026-05-12 00:14:22 +0600

    libs

---
+++ src/libs/utils.js
@@ -0,0 +1,7 @@
+export const cn = (...classes) => classes.filter(Boolean).join(' ')
+
+export const marqueeItems = [
+  'React.js', 'Next.js', 'Node.js', 'MongoDB', 'Tailwind CSS',
+  'Express.js', 'Figma', 'UI/UX Design', 'REST APIs', 'MySQL',
+  'Firebase', 'C / OpenGL', 'JavaScript', 'JWT', 'Git',
+]
